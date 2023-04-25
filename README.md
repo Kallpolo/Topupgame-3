01d892797b46c5daf6a76b1eeb7a67b259f4f405
Kallpolo <mantapjiwa250104@gmail.com>
2023-04-25 17:34:41 +0700

    Topupgame-3

---
+++ README.md
@@ -0,0 +1,3 @@
+![Screenshot](https://github.com/Kallpolo/Topupgame-3/blob/main/demo.jpg)
+
+# [Live Preview](https://kallpolo.github.io/Topupgame-3/)
---
+++ js/main.js
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta content="Alerion Store adalah sebuah marketplace penyedia layanan topup games terbaik #1 Indonesia, dengan harga termurah dan proses super instan. Dapatkan lebih banyak promo dan potongan harga dengan cara bergabung menjadi member / pelanggan Premium." name="description">
+    <meta content="Alerion Store adalah sebuah marketplace penyedia layanan topup games terbaik #1 Indonesia, dengan harga termurah dan proses super instan. Dapatkan lebih banyak promo dan potongan harga dengan cara bergabung menjadi member / pelanggan Premium." name="keyword">
+    <meta name="author" content="Flii">
+        
+    <meta property="og:title" content="Alerion Store">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://www.alerionstore.com">
+    <meta property="og:image" content="../index.html">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:description" content="Alerion Store adalah sebuah marketplace penyedia layanan topup games terbaik #1 Indonesia, dengan harga termurah dan proses super instan. Dapatkan lebih banyak promo dan potongan harga dengan cara bergabung menjadi member / pelanggan Premium.">
+    <meta property="og:site_name" content="Alerion Store">
+    <meta property="og:locale" content="id_ID">
+    <meta property="fb:pages" content>
+    <meta property="fb:admins" content>
+    <meta property="twitter:card" content="Summary">
+    <meta property="twitter:url" content="https://www.alerionstore.com">
+    <meta property="twitter:title" content="Alerion Store">
+    <meta property="twitter:image" content="https://www.alerionstore.com">
+    <meta property="twitter:creator" content="@yb_raf">
+
+    <meta name="apple-mobile-web-app-title" content="Alerion Store adalah sebuah marketplace penyedia layanan topup games terbaik #1 Indonesia, dengan harga termurah dan proses super instan. Dapatkan lebih banyak promo dan potongan harga dengan cara bergabung menjadi member / pelanggan Premium.">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-touch-fullscreen" content="yes">
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="../images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png">
+    <link rel="manifest" href="https://alerionstore.com/assets/favicon/site.webmanifest">
+    <link rel="mask-icon" href="../fonts/safari-pinned-tab.svg" color="#0010f7">
+    <meta name="msapplication-TileColor" content="#0010f7">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Plugin -->
+    <link rel="stylesheet" type="text/css" href="../css/swiper-bundle.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/index.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/index.min_1.css">
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../css/colors.css">
+
+    <!-- Base -->
+    <link rel="stylesheet" type="text/css" href="../css/font-control.css">
+    <link rel="stylesheet" type="text/css" href="../css/typography.css">
+    <link rel="stylesheet" type="text/css" href="../css/base.css">
+
+    <!-- Theme -->
+    <link rel="stylesheet" type="text/css" href="../css/colors-dark.css">
+    <link rel="stylesheet" type="text/css" href="../css/theme-dark.css">
+    <link rel="stylesheet" type="text/css" href="../css/custom-rtl.css">
+
+    <!-- Layouts -->
+    <link rel="stylesheet" type="text/css" href="../css/sider.css">
+    <link rel="stylesheet" type="text/css" href="../css/header.css">
+    <link rel="stylesheet" type="text/css" href="../css/components.css">
+    <!-- Customizer -->
+    <link rel="stylesheet" type="text/css" href="../css/customizer.css">
+
+    <!-- Pages -->
+    <link rel="stylesheet" type="text/css" href="../css/page-error.css">
+
+    <!-- Custom -->
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+
+    <title>Not Found</title>
+
+</head>
+
+<body>
+    <div class="row bg-primary-4 hp-bg-color-dark-90 text-center">
+        <div class="col-12 hp-error-content py-32">
+            <div class="row align-items-center justify-content-center h-100">
+                <div class="col-12">
+
+                    <h1 class="hp-error-content-title mb-0 mb-sm-8 fw-light">404</h1>
+
+                    <h2 class="h1 mb-0 mb-sm-16">Oops, Halaman tidak ditemukan!</h2>
+
+                    <a href="https://alerionstore.com">
+                        <button type="button" class="btn btn-primary">
+                            <span>Halaman Utama</span>
+                        </button>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 py-24">
+            <p class="mb-0 hp-badge-text">COPYRIGHT ©2022 Alerion Store</p>
+        </div>
+    </div>
+
+    <!-- Plugin -->
+    <script src="jquery.min_1.js"></script>
+    <script src="bootstrap.bundle.min.js"></script>
+    <script src="swiper-bundle.min.js"></script>
+    <script src="jquery.mask.min.js"></script>
+    <script src="autocomplete.min.js"></script>
+    <script src="moment.min_1.js"></script>
+
+    <!-- Layouts -->
+    <script src="header-search.js"></script>
+    <script src="sider.js"></script>
+    <script src="input-number.js"></script>
+
+    <!-- Base -->
+    <script src="index.js"></script>
+    <!-- Customizer -->
+    <script src="customizer.js"></script>
+
+    <!-- Custom -->
+    <script src="main.js"></script>
+</body>
+
+</html>
